c934f59f29137ab3d310d7d711a5e0a9093ddf77
EC2 Default User <ec2-user@ip-172-31-13-196.ap-northeast-2.compute.internal>
2022-11-13 03:05:15 +0000

    msk 구현

--- terraform/msk/main.tf
+++ terraform/msk/main.tf
@@ -3,3 +3,11 @@ module "vpc" {
 
   name = "msk"
 }
+
+module "msk" {
+  source = "../modules/msk"
+
+  vpc_id = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
