b138f0759728b2f33d0ba7014b70f5b29403803e
EC2 Default User <ec2-user@ip-172-31-13-196.ap-northeast-2.compute.internal>
2022-11-13 02:20:10 +0000

    토픽 만드는 script 수정

--- scripts/make-topic.sh
+++ scripts/make-topic.sh
@@ -3,5 +3,5 @@
 BOOTSTRAP_SERVER=$1
 TOPIC_NAME=$2
 
-bash ~/kafka/kafka-topics.sh --create --bootstrap-server ${BOOTSTRAP_SERVER} \
+~/kafka/bin/kafka-topics.sh --create --bootstrap-server ${BOOTSTRAP_SERVER} \
 	--replication-factor 2 --partitions 2 --topic ${TOPIC_NAME}
