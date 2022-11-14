18952fe49b884e7743cbe202cb3433f5088fbfe3
euiraekim <harrykur139@gmail.com>
2022-11-14 19:05:44 +0900

    run console consumer 스크립트 파일에서 kafka path 추가

--- scripts/run-console-consumer.sh
+++ scripts/run-console-consumer.sh
@@ -3,5 +3,7 @@
 BOOTSTRAP_SERVER=$1
 TOPIC_NAME=$2
 
-/kafka/bin/kafka-console-consumer.sh --bootstrap-server ${BOOTSTRAP_SERVER} \
---consumer.config client.properties --topic ${TOPIC_NAME} --from-beginning
+KAFKA_PATH="/kafka/bin"
+
+${KAFKA_PATH}/kafka-console-consumer.sh --bootstrap-server ${BOOTSTRAP_SERVER} \
+--consumer.config ${KAFKA_PATH}/client.properties --topic ${TOPIC_NAME} --from-beginning
