e6fe02654cd5873f73f6727dec6a9761ff140aed
EC2 Default User <ec2-user@ip-172-31-13-196.ap-northeast-2.compute.internal>
2022-11-17 00:56:40 +0000

    vpc 모듈 output들 추가

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -2,10 +2,22 @@ output "vpc_id" {
   value = aws_vpc.default.id
 }
 
+output "vpc_cidr" {
+  value = aws_vpc.default.cidr_block
+}
+
 output "public_subnet_ids" {
   value = aws_subnet.public.*.id
 }
 
 output "private_subnet_ids" {
   value = aws_subnet.private.*.id
+}
+
+output "public_route_table_ids" {
+  value = aws_route_table.public.*.id
+}
+
+output "private_route_table_ids" {
+  value = aws_route_table.private.*.id
 }
