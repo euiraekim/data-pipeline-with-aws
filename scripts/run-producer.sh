cdf6059ff8cd10c1a0964f63c0122f9a6c18a091
euiraekim <harrykur139@gmail.com>
2022-11-24 11:32:36 +0000

    run-producer 스크립트 다른 파라미터들도 받게 수정(실수 수정임)

--- scripts/run-producer.sh
+++ scripts/run-producer.sh
@@ -8,4 +8,8 @@ INTER_MESSAGE_SECOND=$5
 INTER_ITER_SECOND=$6
 
 python3 kafka/producer/main.py -bs ${BOOTSTRAP_SERVER} \
-	-iuc ${INIT_USER_COUNT}
+	-iuc ${INIT_USER_COUNT} \
+        -uc ${USER_COUNT_IN_LOOP} \
+        -r ${RATIO_ORDER_USER} \
+        -mt ${INTER_MESSAGE_SECOND} \
+        -it ${INTER_ITER_SECOND}
