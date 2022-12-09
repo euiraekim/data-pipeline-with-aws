17da0bd447a7d76340962502513c75fdc9e40f14
euiraekim <harrykur139@gmail.com>
2022-12-09 14:14:43 +0000

    batch pipeline 관련된 리소스들(emr, redshift, airflow eks)을 하나의 vpc에 들어가도록 수정 (vpc를 region당 5개 이상 만들 수 없기 때문)

--- terraform/redshift/vpc.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source = "../modules/vpc"
-
-  name = "redshift"
-  vpc_cidr = "10.4.0.0/16"
-  public_subnet_cidrs = ["10.4.1.0/24", "10.4.3.0/24"]
-  private_subnet_cidrs = ["10.4.2.0/24", "10.4.4.0/24"]
-}
