cbea97de4602feb94482c0375081b3234942654c
euiraekim <hadoop@ip-10-3-1-221.ap-northeast-2.compute.internal>
2022-12-11 04:38:43 +0000

    users_to_redshift 스파크 코드 airflow에서 받은 datetime 정보를 반영하여 데이터를 가져오게 변경

--- spark/users_to_redshift.py
+++ spark/users_to_redshift.py
@@ -1,12 +1,33 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
+
+import argparse
+from datetime import datetime
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-dt', help='datetime')
+args = parser.parse_args()
+
+dt = datetime.strptime(args.dt, '%Y-%m-%d %H:%M:%S')
+date_str = dt.strftime('%Y-%m-%d')
+hour_str = dt.strftime('%H')
+
 
 spark = SparkSession.builder.appName('user_s3_to_redshift').getOrCreate()
 
-user_df = spark.read.json('s3://euiraekim-kafka-topics/topics/User/*/*/*.json')
+try:
+    user_df = spark.read.json(f's3://euiraekim-kafka-topics/topics/User/{date_str}/{hour_str}/*.json')
 
-redshift_url = 'jdbc:redshift://redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com:5439/redshift_test?user=testuser&password=Testpw1234'
+    split_col = F.split(F.col('email'), '@')
+    user_df = user_df.withColumn('email_user', split_col.getItem(0))
+    user_df = user_df.withColumn('email_domain', split_col.getItem(1))
 
-user_df.write.format('io.github.spark_redshift_community.spark.redshift') \
-    .option('url', redshift_url).option('dbtable', 'users') \
-    .option('tempdir', 's3://euiraekim-redshift-temp/temp') \
-    .option("forward_spark_s3_credentials", "true").mode('append').save()
+    redshift_url = 'jdbc:redshift://redshift-test.cyernhele58c.ap-northeast-2.redshift.amazonaws.com:5439/redshift_test?user=testuser&password=Testpw1234'
+
+    user_df.write.format('io.github.spark_redshift_community.spark.redshift') \
+        .option('url', redshift_url).option('dbtable', 'users') \
+        .option('tempdir', 's3://euiraekim-redshift-temp/temp') \
+        .option("forward_spark_s3_credentials", "true").mode('append').save()
+
+except Exception as e:
+    print(e)
