64472b1771e3a55dad66d4f4083364b6d683c885
euiraekim <harrykur139@gmail.com>
2022-12-12 11:39:15 +0000

    postgre init 파일 create table 쿼리들 추가 (아마 최종)

--- db/init_postgresql.py
+++ db/init_postgresql.py
@@ -1,15 +1,74 @@
 from connection import get_postgresql_conn
 
-create_table_query = """
-    CREATE TABLE sign_up_count (
-        test VARCHAR(50) NOT NULL
-    )
-"""
+create_table_queries = [
+            """
+                CREATE TABLE user_count (
+                    date TIMESTAMP,
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE user_address_count (
+                    date TIMESTAMP,
+                    address VARCHAR(50),
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE user_email_domain_count (
+                    date TIMESTAMP,
+                    email_domain VARCHAR(50),
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE user_gender_count (
+                    date TIMESTAMP,
+                    gender CHAR(5),
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE order_count (
+                    date TIMESTAMP,
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE order_category_count (
+                    date TIMESTAMP,
+                    first_category VARCHAR(50),
+                    second_category VARCHAR(50),
+                    count INT
+                );
+            """,
+            """
+                CREATE TABLE order_sales (
+                    date TIMESTAMP,
+                    amount INT
+                ) 
+            """,
+            """
+                CREATE TABLE order_address_count (
+                    date TIMESTAMP,
+                    address VARCHAR(50),
+                    count INT
+                )
+            """,
+            """
+                CREATE TABLE order_gender_count (
+                    date TIMESTAMP,
+                    gender VARCHAR(50),
+                    count INT
+                )
+            """
+        ]
 
 conn = get_postgresql_conn()
 
 with conn.cursor() as cursor:
     try:
-        cursor.execute(create_table_query)
+        for query in create_table_queries:
+            cursor.execute(query)
     except Exception as e:
         print(e)
